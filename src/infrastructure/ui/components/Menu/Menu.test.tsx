1315457e3f7bb43c3db3ebb35e4d3ef467700b4d
Ulises Santana <ulisesantana@gmail.com>
2022-09-30 00:51:37 +0100

    ✅ add test for Menu

---
+++ src/infrastructure/ui/components/Menu/Menu.test.tsx
@@ -0,0 +1,55 @@
+import {Views} from "../../App/App";
+import {Menu, MenuProps, messages} from "./Menu";
+import {render, screen} from "@testing-library/react";
+import {messages as searchMessages} from '../Search'
+import userEvent from "@testing-library/user-event";
+
+describe('Menu should', () => {
+  it.each([
+    {action: 'go to all items list', label: messages.allItemsListCTA},
+    {action: 'go to buy list', label: messages.requiredListCTA},
+    {action: 'go to mandatory buy list', label: messages.mandatoryListCTA},
+    {action: 'enable search', label: messages.searchCTA},
+  ])('show an option for $action', ({label}) => {
+    const props = buildMenuProps()
+    render(<Menu {...props}/>)
+
+    screen.getByLabelText(label)
+  });
+  it.each([
+    {action: 'go to all items list', label: messages.allItemsListCTA, expectedView: Views.All},
+    {action: 'go to buy list', label: messages.requiredListCTA, expectedView: Views.Required},
+    {action: 'go to mandatory buy list', label: messages.mandatoryListCTA, expectedView: Views.Mandatory},
+  ])('$action', ({label, expectedView}) => {
+    const props = buildMenuProps()
+    render(<Menu {...props}/>)
+
+    userEvent.click(screen.getByLabelText(label))
+
+    expect(props.setView).toHaveBeenCalledWith(expectedView)
+  });
+
+  it('not show search bar if search is not enabled', () => {
+    const props = buildMenuProps()
+    render(<Menu {...props}/>)
+
+    expect(screen.queryByLabelText(searchMessages.searchInput)).toBeNull()
+  })
+
+  it('search items if search is enabled', () => {
+    const props = buildMenuProps()
+    render(<Menu {...props}/>)
+
+    userEvent.click(screen.getByLabelText(messages.searchCTA))
+
+   screen.getByLabelText(searchMessages.searchInput)
+  })
+});
+
+function buildMenuProps(activeView = Views.All): MenuProps {
+  return {
+    activeView,
+    onSearch: jest.fn(),
+    setView: jest.fn(),
+  }
+}
---
+++ src/infrastructure/ui/components/Menu/Menu.tsx
@@ -0,0 +1,63 @@
+import React, {FC, useState} from "react";
+import './Menu.scss'
+import {Views} from "../../App/App";
+import {BsFillCartFill, BsFillStarFill, BsSearch} from "react-icons/bs";
+import {FiList} from "react-icons/fi";
+import {palette} from "../../../../core";
+import {Search} from "../Search";
+
+export interface MenuProps {
+  setView: (view: Views) => void;
+  activeView: Views;
+  onSearch: (search: string) => void
+}
+
+export const messages = {
+  requiredListCTA: 'Show items to buy',
+  mandatoryListCTA: 'Show mandatory items to buy',
+  allItemsListCTA: 'Show all items',
+  searchCTA: 'Search items'
+}
+
+export const Menu: FC<MenuProps> = ({setView, activeView, onSearch}) => {
+  const [search, setSearch] = useState(false)
+  const isActive = (view: Views) => view === activeView
+  const getBackgroundColor = (view: Views) => isActive(view) ? palette.yellow : 'transparent'
+  const getTextColor = (view: Views) => isActive(view) ? palette.purple : 'inherit'
+  return (
+    <nav className="Menu">
+      <div className={`flip-container${search ? ' active' : ''}`}>
+        {search
+          ? <div className={`flip-back${search ? ' active' : ''}`}>
+            <Search onChange={onSearch} onClose={() => setSearch(false)}/>
+          </div>
+          : <ul
+            style={{backgroundColor: palette.purple, color: palette.white}}
+            className={`flip-front${!search ? ' active' : ''}`}
+          >
+            <li style={{backgroundColor: getBackgroundColor(Views.All), color: getTextColor(Views.All)}}>
+              <button aria-label={messages.allItemsListCTA} onClick={() => setView(Views.All)}>
+                <FiList/>
+              </button>
+            </li>
+            <li style={{backgroundColor: getBackgroundColor(Views.Required), color: getTextColor(Views.Required)}}>
+              <button aria-label={messages.requiredListCTA} onClick={() => setView(Views.Required)}>
+                <BsFillCartFill/>
+              </button>
+            </li>
+            <li style={{backgroundColor: getBackgroundColor(Views.Mandatory), color: getTextColor(Views.Mandatory)}}>
+              <button aria-label={messages.mandatoryListCTA} onClick={() => setView(Views.Mandatory)}>
+                <BsFillStarFill/>
+              </button>
+            </li>
+            <li>
+              <button aria-label={messages.searchCTA} onClick={() => setSearch(true)}>
+                <BsSearch/>
+              </button>
+            </li>
+          </ul>
+        }
+      </div>
+    </nav>
+  )
+}
