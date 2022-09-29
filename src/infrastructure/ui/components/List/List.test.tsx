567f4fa2d6d6051c0f97b5f075d27c05dbfc6742
Ulises Santana <ulisesantana@gmail.com>
2022-09-29 23:29:36 +0100

    ✅ add test for List

---
+++ src/infrastructure/ui/components/List/List.test.tsx
@@ -0,0 +1,31 @@
+import {ItemBuilder} from "../../../../tests/builders/ItemBuilder";
+import {List, ListProps, messages} from "./List";
+import {render, screen} from "@testing-library/react";
+
+describe('List should', () => {
+  it('show a list of items separated by categories', () => {
+    const props = buildListProps()
+    render(<List {...props}/>)
+
+    props.items.forEach(item => {
+      screen.getByText(item.name)
+      screen.getByText(item.category.name)
+    })
+  });
+  it('show a message if there is no items to show', () => {
+    const props = buildListProps([])
+    render(<List {...props}/>)
+
+    screen.getByText(messages.emptyList)
+  });
+});
+
+export function buildListProps(items = Array(3).map(ItemBuilder.random)): ListProps {
+  return {
+    items,
+    setItemAsRequired: jest.fn(),
+    setItemAsNotRequired: jest.fn(),
+    setItemAsMandatory: jest.fn(),
+    setItemAsNotMandatory: jest.fn(),
+  }
+}
--- src/infrastructure/ui/components/List/List.tsx
+++ src/infrastructure/ui/components/List/List.tsx
@@ -1,16 +1,20 @@
 import {FC} from "react";
-import {Id, Item} from "../../../core";
-import {ListItem} from "./ListItem";
+import {Id, Item} from "../../../../core";
+import {ListItem} from "../ListItem";
 import './List.scss'
 
 type SetItemFunction = (id: Id) => void
 
-interface ItemListProps {
+export interface ListProps {
   items: Item[],
   setItemAsRequired: SetItemFunction,
   setItemAsNotRequired: SetItemFunction,
   setItemAsMandatory: SetItemFunction,
   setItemAsNotMandatory: SetItemFunction,
+}
+
+export const messages = {
+  emptyList:'There is no items in this list',
 }
 
 function getItemsByCategory(items: Item[]) {
@@ -25,15 +29,16 @@ function getItemsByCategory(items: Item[]) {
 }
 
 const EmptyList = () => <div className="List">
-  <p>No hay elementos</p>
+  <p>{messages.emptyList}</p>
 </div>
 
-export const List: FC<ItemListProps> = ({items, ...useCases}) => {
-  if (!items) {
+export const List: FC<ListProps> = ({items, ...useCases}) => {
+  if (items.length === 0) {
     return <EmptyList />
   }
   return <div className="List">
-    {Object.entries(getItemsByCategory(items)).map(([categoryName, items]) => <div key={categoryName}>
+    {Object.entries(getItemsByCategory(items)).map(([categoryName, items]) =>
+      <div key={categoryName}>
         <h2 className="category-name">{categoryName}</h2>
         <ul className="ItemList">
           {items.map(item => (
