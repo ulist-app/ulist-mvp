73e8f54926aaa5ee0fd5d94919d570e3085c297a
Ulises Santana <ulisesantana@gmail.com>
2022-09-30 22:14:24 +0100

    ♿️ use button for CTA instead of svg

--- src/infrastructure/ui/components/ListItem/ListItem.tsx
+++ src/infrastructure/ui/components/ListItem/ListItem.tsx
@@ -16,23 +16,31 @@ export interface ListItemProps {
 }
 
 export const ListItem: FC<ListItemProps> = ({item, ...useCases}) => {
-  return <div className="ListItem">
+  return <div className="ListItem" data-testid={item.id.value}>
     <span className="quantity">{item.quantity}</span>
     <span className="item">{item.name}</span>
     <span className="is-required">
       {item.isRequired
-        ? <AiFillMinusCircle aria-label={messages.actions.setItemAsNotRequired} size={32} color={palette.red}
-                             onClick={() => useCases.setItemAsNotRequired(item.id)}/>
-        : <AiFillPlusCircle aria-label={messages.actions.setItemAsRequired} size={32} color={palette.green}
-                            onClick={() => useCases.setItemAsRequired(item.id)}/>
+        ? <button aria-label={messages.actions.setItemAsNotRequired}
+                  onClick={() => useCases.setItemAsNotRequired(item.id)}>
+          <AiFillMinusCircle size={32} color={palette.red}/>
+        </button>
+        : <button aria-label={messages.actions.setItemAsRequired}
+                  onClick={() => useCases.setItemAsRequired(item.id)}>
+          <AiFillPlusCircle size={32} color={palette.green}/>
+        </button>
       }
     </span>
     <span className="is-mandatory">
       {item.isMandatory
-        ? <BsFillStarFill aria-label={messages.actions.setItemAsNotMandatory} size={32} color={palette.yellow}
-                          onClick={() => useCases.setItemAsNotMandatory(item.id)}/>
-        : <BsFillStarFill aria-label={messages.actions.setItemAsMandatory} size={32} color={palette.gray}
-                          onClick={() => useCases.setItemAsMandatory(item.id)}/>
+        ? <button aria-label={messages.actions.setItemAsNotMandatory}
+                  onClick={() => useCases.setItemAsNotMandatory(item.id)}>
+          <BsFillStarFill size={32} color={palette.yellow}/>
+        </button>
+        : <button aria-label={messages.actions.setItemAsMandatory}
+                  onClick={() => useCases.setItemAsMandatory(item.id)}>
+          <BsFillStarFill size={32} color={palette.gray}/>
+        </button>
       }
     </span>
   </div>
